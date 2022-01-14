5a599307e473c347d8e4fd168e4005f7433e6790
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-15 00:36:35 +0200

    add text theme for headline 5 and subtitle 2

--- lib/views/utils/theme_cubit.dart
+++ lib/views/utils/theme_cubit.dart
@@ -7,9 +7,23 @@ class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit() : super(_theme);
 
   static final _theme = ThemeData(
-      brightness: Brightness.light,
-      primaryColor: kAccentColor,
-      iconTheme: IconThemeData(
-        size: 25.0,
-      ));
+    brightness: Brightness.light,
+    primaryColor: kAccentColor,
+    iconTheme: IconThemeData(
+      size: 25.0,
+    ),
+    textTheme: TextTheme(
+      headline5: TextStyle(
+        fontSize: 18.0,
+        color: Colors.black,
+        fontWeight: FontWeight.w400,
+        height: 1.1,
+      ),
+      subtitle2: TextStyle(
+        fontSize: 14.0,
+        color: Color(0xFF7C7C7C),
+        fontWeight: FontWeight.w400,
+      ),
+    ),
+  );
 }
