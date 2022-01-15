dd057bad6d4ec561882929a99fa512790e6c1df2
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-15 09:30:52 +0200

    change constructor to get storageRepository

--- lib/views/home/bloc/home_bloc.dart
+++ lib/views/home/bloc/home_bloc.dart
@@ -3,11 +3,16 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '/services/storage_service/storage_repository.dart';
+
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial());
+  final StorageRepository storageRepository;
+  HomeBloc({
+    required this.storageRepository,
+  }) : super(HomeInitial());
 
   @override
   Stream<HomeState> mapEventToState(
