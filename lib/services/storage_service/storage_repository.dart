3019ed8389429ae60d7883e8e26dec6be2516fd5
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-15 09:23:06 +0200

    create fetchArticleBySectionAndPeriod function to get the article by section and period

--- lib/services/storage_service/storage_repository.dart
+++ lib/services/storage_service/storage_repository.dart
@@ -1,1 +1,6 @@
-abstract class StorageRepository {}
+import 'package:ny_times_test/models/article.dart';
+
+abstract class StorageRepository {
+  Stream<List<Article>> fetchArticleBySectionAndPeriod(
+      {required String section, required String period});
+}
