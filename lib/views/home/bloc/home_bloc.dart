5e584bdb1e1ed1e730ad9e82e53b27b561e97bde
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-15 08:18:17 +0200

    initial bloc class with state and event

---
+++ lib/views/home/bloc/home_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial());
+
+  @override
+  Stream<HomeState> mapEventToState(
+    HomeEvent event,
+  ) async* {}
+
+  @override
+  Future<void> close() {
+    return super.close();
+  }
+}
---
+++ lib/views/home/bloc/home_event.dart
@@ -0,0 +1,7 @@
+part of 'home_bloc.dart';
+
+abstract class HomeEvent extends Equatable {
+  HomeEvent();
+  @override
+  List<Object> get props => [];
+}
