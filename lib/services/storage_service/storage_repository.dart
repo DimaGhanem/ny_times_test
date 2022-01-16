fd9ccb961f5b16cb1bebcfbeb914984562bcb958
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-16 16:33:07 +0200

    Add function to do get the result and return it as ResponseModel

--- lib/services/storage_service/storage_repository.dart
+++ lib/services/storage_service/storage_repository.dart
@@ -1,6 +1,10 @@
 import 'package:ny_times_test/models/article.dart';
+import 'package:ny_times_test/models/response_model.dart';
 
 abstract class StorageRepository {
   Future<List<Article>> fetchArticleBySectionAndPeriod(
+      {required String section, required String period});
+
+  Future<ResponseModel> fetchArticleBySectionAndPeriodWithResponse(
       {required String section, required String period});
 }
