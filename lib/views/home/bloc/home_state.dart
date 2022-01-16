8dd5af95adf2032b7d72e51d1e12b4375b72892d
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-16 09:50:56 +0200

    Add ArticlesLoadSuccess State  that returned articles

--- lib/views/home/bloc/home_state.dart
+++ lib/views/home/bloc/home_state.dart
@@ -9,3 +9,13 @@ abstract class HomeState extends Equatable {
 class HomeInitial extends HomeState {}
 
 class ArticlesLoading extends HomeState {}
+
+class ArticlesLoadSuccess extends HomeState {
+  final List<Article> articles;
+
+  const ArticlesLoadSuccess(this.articles)
+      : assert(articles != null, "field must equal value");
+
+  @override
+  List<Object> get props => [articles];
+}
