576d6ed0202ed7ee952eaa519d40ace1a79c5cfd
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-16 09:40:23 +0200

    Initial dio and add timeout options and add  interceptor LoggingInterceptor

--- lib/services/article_api_provider.dart
+++ lib/services/article_api_provider.dart
@@ -1,10 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:ny_times_test/models/response_model.dart';
 
+import '../utils/logging_interceptor.dart';
 
 class ArticleApiProvider {
-
+  late Dio _dio;
 
   ArticleApiProvider() {
-
+    BaseOptions options =
+    BaseOptions(receiveTimeout: 5000, connectTimeout: 5000);
+    _dio = Dio(options);
+    _dio.interceptors.add(LoggingInterceptor());
   }
 
 
