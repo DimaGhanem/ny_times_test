d215eccc89eb7e84ec4cc0dc11fffecd9fd366c4
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-16 16:37:57 +0200

    Create test to check if the article length is not 0 when the response is ok

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,5 @@
+import 'package:ny_times_test/models/article.dart';
+import 'package:ny_times_test/models/response_model.dart';
 import 'package:ny_times_test/services/storage_service/storage_repo_impl.dart';
 import 'package:test/test.dart';
 
@@ -6,5 +8,15 @@ void main() {
 
   setUp(() {
     repository = StorageRepoImpl();
+  });
+
+  //success
+  test('list of article length should not be 0 for this period ', () async {
+    //act
+    List<Article> result = await repository.fetchArticleBySectionAndPeriod(
+        section: 'all-sections', period: '7');
+
+    //assert
+    expect(result.length != 0, true);
   });
 }
