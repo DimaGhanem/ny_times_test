7b1c72095cb51830c5f1007188f0a3e032f806e3
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-16 10:00:57 +0200

    Create ArticleListWidget widget that stream the state from HomeBloc and create _mapStateToActions to map the state with actions

--- lib/views/home/widget/home_screen.dart
+++ lib/views/home/widget/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ny_times_test/models/article.dart';
+import 'package:ny_times_test/views/home/bloc/home_bloc.dart';
 
 import 'drawer_widget.dart';
 
@@ -28,6 +31,44 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: Container());
+        body: ArticleListWidget());
+  }
+}
+
+class ArticleListWidget extends StatefulWidget {
+  ArticleListWidget({Key? key}) : super(key: key);
+
+  @override
+  _ArticleListWidgetState createState() => _ArticleListWidgetState();
+}
+
+class _ArticleListWidgetState extends State<ArticleListWidget> {
+  // late HomeBloc homeBloc;
+
+  List<Article> articles = [];
+  @override
+  void initState() {
+    // homeBloc = BlocProvider.of<HomeBloc>(context)..add(ArticlesStartFetching());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<HomeState>(
+      stream: BlocProvider.of<HomeBloc>(context).handleArticlesStartFetching(),
+      builder: (context, snapshot) {
+        final state = snapshot.data;
+        if (state != null) {
+          _mapStateToActions(state);
+        }
+        return Container(child: Text('Article length  ${articles.length}'));
+      },
+    );
+  }
+
+  void _mapStateToActions(HomeState state) {
+    if (state is ArticlesLoadSuccess) {
+      articles = state.articles;
+    }
   }
 }
