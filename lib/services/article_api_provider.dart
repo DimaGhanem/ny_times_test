676dd1954a72355a3dc820a7b483f53295372ea5
Dima Ghanem <Dima.M.Ghanem@hotmail.com>
2022-01-16 09:42:03 +0200

    create handleError function that returned message describe the happened error

--- lib/services/article_api_provider.dart
+++ lib/services/article_api_provider.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:ny_times_test/models/response_model.dart';
 
 import '../utils/logging_interceptor.dart';
 
@@ -7,8 +8,40 @@ class ArticleApiProvider {
 
   ArticleApiProvider() {
     BaseOptions options =
-        BaseOptions(receiveTimeout: 5000, connectTimeout: 5000);
+    BaseOptions(receiveTimeout: 5000, connectTimeout: 5000);
     _dio = Dio(options);
     _dio.interceptors.add(LoggingInterceptor());
+  }
+
+  String _handleError({required error}) {
+    String errorDescription = "";
+    if (error is DioError) {
+      DioError dioError = error as DioError;
+      switch (dioError.type) {
+        case DioErrorType.cancel:
+          errorDescription = "Request to API server was cancelled";
+          break;
+        case DioErrorType.connectTimeout:
+          errorDescription = "Connection timeout with API server";
+          break;
+        case DioErrorType.other:
+          errorDescription =
+          "Connection to API server failed due to internet connection";
+          break;
+        case DioErrorType.receiveTimeout:
+          errorDescription = "Receive timeout in connection with API server";
+          break;
+        case DioErrorType.response:
+          errorDescription =
+          "Received invalid status code: ${dioError.response?.statusCode}";
+          break;
+        case DioErrorType.sendTimeout:
+          errorDescription = "Send timeout in connection with API server";
+          break;
+      }
+    } else {
+      errorDescription = "Unexpected error occured";
+    }
+    return errorDescription;
   }
 }
